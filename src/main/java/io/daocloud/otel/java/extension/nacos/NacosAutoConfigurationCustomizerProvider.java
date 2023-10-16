8e77276c9ae10a31a867cdb03de5f61e0d524a64
Zhuohao Li <lizhuohao1996@gmail.com>
2023-10-16 19:02:03 +0800

    add nacos properties (#4)
    * add nacos properties
    
    * fix  smell

--- src/main/java/io/daocloud/otel/java/extension/nacos/NacosAutoConfigurationCustomizerProvider.java
+++ src/main/java/io/daocloud/otel/java/extension/nacos/NacosAutoConfigurationCustomizerProvider.java
@@ -6,13 +6,17 @@
 package io.daocloud.otel.java.extension.nacos;
 
 import com.google.auto.service.AutoService;
+import io.daocloud.otel.java.extension.kube.KubeClient;
+import io.kubernetes.client.openapi.models.V1Namespace;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
 import io.opentelemetry.sdk.trace.SpanLimits;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * This is one of the main entry points for Instrumentation Agent's customizations. It allows
@@ -25,29 +29,47 @@ import java.util.Map;
  */
 @AutoService(AutoConfigurationCustomizerProvider.class)
 public class NacosAutoConfigurationCustomizerProvider
-    implements AutoConfigurationCustomizerProvider {
+        implements AutoConfigurationCustomizerProvider {
+    final static String SPRING_CLOUD_DEP = "com.alibaba.cloud.nacos.discovery.NacosDiscoveryAutoConfiguration";
 
-  @Override
-  public void customize(AutoConfigurationCustomizer autoConfiguration) {
-    autoConfiguration
-        .addTracerProviderCustomizer(this::configureSdkTracerProvider)
-        .addPropertiesSupplier(this::getDefaultProperties);
-  }
+    @Override
+    public void customize(AutoConfigurationCustomizer autoConfiguration) {
+        autoConfiguration
+                .addTracerProviderCustomizer(this::configureSdkTracerProvider)
+                .addPropertiesSupplier(this::getNacosProperties);
+    }
 
-  private SdkTracerProviderBuilder configureSdkTracerProvider(
-      SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
+    private SdkTracerProviderBuilder configureSdkTracerProvider(
+            SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
 
-    return tracerProvider
-        .setSpanLimits(SpanLimits.builder().setMaxNumberOfAttributes(1024).build());
-  }
+        return tracerProvider
+                .setSpanLimits(SpanLimits.builder().setMaxNumberOfAttributes(1024).build());
+    }
 
-  private Map<String, String> getDefaultProperties() {
-    Map<String, String> properties = new HashMap<>();
-    //TODO: add nacos custom properties.
-    properties.put("otel.exporter.otlp.endpoint", "http://backend:8080");
-    properties.put("otel.exporter.otlp.insecure", "true");
-    properties.put("otel.config.max.attrs", "16");
-    properties.put("otel.traces.sampler", "demo");
-    return properties;
-  }
+    private Map<String, String> getNacosProperties() {
+        Map<String, String> properties = new HashMap<>();
+
+        KubeClient kubeController = new KubeClient();
+        V1Namespace ns;
+        try {
+            ns = kubeController.getNamespace("kube-system");
+        } catch (Exception e) {
+            System.err.printf("java-client get namespace error: %s", e.getMessage());
+            return properties;
+        }
+
+        String clusterId = Objects.isNull(ns.getMetadata().getUid()) ? "" : ns.getMetadata().getUid();
+        String podName = Objects.isNull(System.getenv("OTEL_RESOURCE_ATTRIBUTES_POD_NAME")) ? "" : System.getenv("OTEL_RESOURCE_ATTRIBUTES_POD_NAME");
+        String k8sNamespace = Objects.isNull(System.getenv("K8S_NAMESPACE")) ? "" : System.getenv("K8S_NAMESPACE");
+
+        try {
+            Class.forName(SPRING_CLOUD_DEP);
+            properties.put("spring.cloud.nacos.discovery.metadata.k8s_cluster_id", clusterId);
+            properties.put("spring.cloud.nacos.discovery.metadata.k8s_namespace_name", k8sNamespace);
+            properties.put("spring.cloud.nacos.discovery.metadata.k8s_pod_name", podName);
+        } catch (Exception ignored) {
+        }
+
+        return properties;
+    }
 }
